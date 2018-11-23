7bb755d8985ec3357f708768e8497fc7d0ae006f
HrFlink <henrik@dan-mail.dk>
2018-11-23 17:47:38 +0100

    First commit

--- app/src/main/java/dk/voresweb/voresvin/MainActivity.java
+++ app/src/main/java/dk/voresweb/voresvin/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mResultTextView;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
